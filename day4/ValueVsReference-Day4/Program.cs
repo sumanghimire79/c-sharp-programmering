70ac6d19c315d65dbc7b7c96f2d495db7b0f27dc
sumanghimire79 <sumanghimire79@yahoo.com>
2023-11-05 20:00:10 +0100

    value type vs reference type hw

---
+++ day4/ValueVsReference-Day4/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Xml.Linq;
+
+namespace ValueVsReference_Day4
+{
+
+    class person
+    {
+        public int ID  { get; set; }
+        public string name { get; set; }
+    }
+
+
+
+    class ClassCustomer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public ClassCustomer(int ID, string Name)
+        {
+            this.ID = ID;
+            this.Name = Name;
+        }
+        public void ClassCustomerInfo()
+        {
+            Console.WriteLine($"Info inside class: ID: {ID}\t Name: {Name}");
+        }
+    }
+
+    struct StructCustomer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public StructCustomer(int ID, string Name)
+        {
+            this.ID = ID;
+            this.Name = Name;
+        }
+        public void StructCustomerInfo()
+        {
+            Console.WriteLine($"Info inside STRUCT: ID: {ID}\t Name: {Name}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = x;
+            Console.WriteLine($"\n x: {x} \t y: {y}");
+            x = 7;
+            Console.WriteLine($"\n x: {x} \t y: {y}");
+
+            Console.WriteLine("----------------");
+            person p1 = new person();
+                person p2 = p1;
+            p1.ID = 1;
+            p1.name = "suman";
+            Console.WriteLine($" p1.name: {p1.name} p2.name: {p2.name}");
+            p2.name = "chanda";
+            Console.WriteLine($" p1.name: {p1.name} p2.name: {p2.name}");
+            
+            
+            person p3 = new person();
+
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
