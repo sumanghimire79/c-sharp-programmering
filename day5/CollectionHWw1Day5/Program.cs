0d314f280464582c885a681beb3ffd8ee20d0da9
sumanghimire79 <sumanghimire79@yahoo.com>
2023-11-07 23:11:26 +0100

    day5 collecteion hw

---
+++ day5/CollectionHWw1Day5/Program.cs
@@ -0,0 +1,132 @@
+﻿namespace CollectionHWw1Day5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region exercise 1
+
+            //a
+            Console.WriteLine("---------1a-----");
+            List<string> friendlist = new List<string>();
+            friendlist.Add("Michael");
+            friendlist.Add("Jonny");
+            friendlist.Add("Lasse");
+            friendlist.Add("Lasemin");
+            friendlist.Add("Jonathan");
+            friendlist.Add("Mich");
+            friendlist.Add("Brian");
+            friendlist.Add("Michail");
+            friendlist.Add("Josepine");
+            friendlist.Add("Suman");
+            friendlist.Sort();
+
+            foreach (var friend in friendlist)
+            {
+                Console.WriteLine(friend);
+            }
+            //b
+            Console.WriteLine("---------1b-----");
+            List<string> SwimminghobbyList = new List<string> { " swimming is healthy", "swimming is fun", " smimming makes man slim", "swimming makes man fresh" };
+            foreach (var hobby in SwimminghobbyList)
+            {
+                Console.WriteLine($" hobby : ---{hobby}");
+            }
+            #endregion
+
+            #region exercise 2
+            //a
+            Console.WriteLine("---------2a-----");
+            List<string> dinnerList = new List<string> { "Lasse", "Brian", "Lasse" };
+            foreach (var friendforDinner in dinnerList)
+            {
+
+                Console.WriteLine($"hey, {friendforDinner} i would like to invie you for dinner !!!");
+
+            }
+            //b
+            Console.WriteLine("---------2b-----");
+            dinnerList[2] = "suman";
+
+            foreach (var friendforDinner in dinnerList)
+            {
+                Console.WriteLine($"hey, {friendforDinner} i would like to invie you for dinner !!!");
+            }
+
+            //c
+            Console.WriteLine("---------2c-----");
+            dinnerList.Add("Ram");
+            dinnerList.Add("Chanda");
+            dinnerList.Add("Robin");
+
+            foreach (var friendforDinner in dinnerList)
+
+            {
+                Console.WriteLine($"hey, {friendforDinner} i would like to invie you for dinner !!!");
+            }
+
+            //d
+            Console.WriteLine("---------2d-----");
+            dinnerList.RemoveAt(dinnerList.Count - 1);
+            dinnerList.RemoveAt(dinnerList.Count - 1);
+            dinnerList.RemoveAt(dinnerList.Count - 1);
+            foreach (var friendforDinner in dinnerList)
+
+
+            {
+                Console.WriteLine($"hey, {friendforDinner} i would like to invie you for dinner !!!");
+            }
+            #endregion
+
+            #region exercise 3: Dictionaries
+
+            //a
+            Console.WriteLine("---------3a-----");
+            Dictionary<string, string> personsInfo = new Dictionary<string, string>();
+            personsInfo.Add("name", "Lasse");
+            personsInfo.Add("age", "44");
+            personsInfo.Add("shower per week", "7");
+
+            foreach (var person in personsInfo)
+            {
+                Console.WriteLine($"{person.Key}:{person.Value}");
+
+            }
+
+            //b
+            Console.WriteLine("---------3b-----");
+            Dictionary<string, int> favouriteNumberList = new Dictionary<string, int>();
+
+            favouriteNumberList.Add("suman", 33);
+            favouriteNumberList.Add("Chanda", 55);
+            favouriteNumberList.Add("Aayusma", 11);
+            favouriteNumberList.Add("Ram", 171);
+            foreach (var favouriteNumber in favouriteNumberList)
+            {
+                if (favouriteNumber.Value < 100)
+                {
+
+                    Console.WriteLine(favouriteNumber.Key);
+                }
+            }
+
+
+            //C
+            Console.WriteLine("---------3c-----");
+
+            Dictionary<string, string> glossary = new Dictionary<string, string>();
+            glossary.Add("c#", " This is popular language in backend programing in denmark");
+            glossary.Add("JS", " JS is popular frontend language that changes the behaviour of a web page");
+            glossary.Add("CSS", " CSS means casceding stylesheet language");
+
+            foreach (var words in glossary)
+            {
+                Console.WriteLine($" \n {words.Key} - means: {words.Value}");
+            }
+
+            Console.ReadLine();
+
+            #endregion
+        }
+    }
+}
---
+++ opgaver/Uge 1 Fredag.txt
@@ -0,0 +1,38 @@
+--- Exercise 1: Lists ---
+a. Store the names of a few of your friends in a list.
+   Print each person's name one at a time.
+
+b. Think of a hobby of yours, and make a list that stores several examples of items from that hobby.
+   Use your list to print a series of statements about these items, such as "Squirtle is a pretty cool dude."
+
+
+
+--- Exercise 2: Modifying Lists ---
+a. If you could invite anyone, living or otherwise, to dinner, who would you invite?
+   Make a list that includes at least three people you'd like to invite to dinner.
+   Use your list to print a message to each person, inviting them to dinner.
+
+b. You just heard that one of your guests can't make the dinner :(
+   You'll have to think of someone else to invite. Modify your list, 
+   replacing the name of the guest who can't make it with the name of a new person.
+   Print an invitation for your new guest.
+
+c. You just found a bigger dinner table! Think of three more guests to invite.
+   Add your new guests to the end of your list.
+
+d. Oh no! You just found out that your new dinner table won't arrive in time.
+   Find a way to uninvite the new guests from 2-c.
+
+
+
+--- Exercise 3: Dictionaries ---
+a. Use a dictionary to store information about a person you know.
+   You decide the information, but classics include first name, age, and showers per week.
+   Print a little message about your friend, using the stored information.
+
+b. Use a dictionary to store the names of a few people you know, along with their favourite numbers.
+   Print some of the people in your dictionary, along with their favourite number.
+
+c. Let's use a dictionary as an actual dictionary (or you could call it a glossary to avoid confusion).
+   Think of five programming words you've learned about, and store them along with their meanings.
+   Print these words and their meanings as a neatly formatted output.
